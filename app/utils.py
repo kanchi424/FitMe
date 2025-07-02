8ca67822b163b9df1fc32d78525dcb16b6307b5a
kanchi424 <64429781+kanchi424@users.noreply.github.com>
2025-07-02 20:05:06 +0530

    Delete app directory

--- app/utils.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-import pytz
-
-def convert_timezone(dt: datetime, target_timezone: str) -> datetime:
-    ist = pytz.timezone("Asia/Kolkata")
-    target = pytz.timezone(target_timezone)
-    dt_ist = ist.localize(dt)
-    return dt_ist.astimezone(target)
